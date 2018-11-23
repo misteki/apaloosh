0903bb39fe0f38a1cbc7509c443b8b88ab2b6955
Matías Cincunegui <mcincunegui.dev@gmail.com>
2018-11-23 16:24:18 -0300

    FOV prototype

--- src/engine/camera.ts
+++ src/engine/camera.ts
@@ -49,20 +49,8 @@ const get_fov_map = (map, origin_x, origin_y, width, height, center_x, center_y)
     const camera_y_origin = Math.floor(origin_y / TILE_SIZE);
     const pov_x = camera_x_origin + center_x;
     const pov_y = camera_y_origin + center_y;
-    const paths = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    let count = 0;
-    // Build checklist
-    const cells_to_check = [];
-    for (let x = camera_x_origin; x < camera_x_origin + width; x++) {
-        cells_to_check.push([x, camera_y_origin]);
-        cells_to_check.push([x, camera_x_origin + width]);
-    };
-    for (let y = camera_y_origin + 1; y < camera_y_origin + height - 1; y++) {
-        cells_to_check.push([camera_x_origin, y]);
-        cells_to_check.push([camera_x_origin + width, y]);
-    }
-    trace(cells_to_check.toString());
-
+    const max_distance = 10;
+    //const max_distance_y = 8;
     //Cast rays
     const place_fov_flag = (x, y, value) => {
         if (!fov_map[x]) {
@@ -72,48 +60,28 @@ const get_fov_map = (map, origin_x, origin_y, width, height, center_x, center_y)
             fov_map[x][y] = value;
         }
     }
-    cells_to_check.forEach(cell => {
-        const goal_x = cell[0];
-        const goal_y = cell[1]
-        let path_x = pov_x;
-        let path_y = pov_y;
-        let visible = true;
-        let count = 0;
-        trace('ray!');
-        trace(`goal: ${goal_x},${goal_y}`)
-        while (path_x !== goal_x || path_y !== goal_y) {
-            const distances = paths
-                .map(path => [...path, get_linear_distance(goal_x, goal_y, path_x + path[0], path_y + path[1])])
-                .sort((a, b) => a[2] - b[2]);
-            const shortest = distances[0];
-
-            trace(`${path_x}, ${path_y}`);
-            if (!visible) {
-                place_fov_flag(path_x, path_y, false);
-            } else {
-                if (get_tile(map, path_x, path_y).flags[TileFlags.OPAQUE]) {
-                    trace(`opaque at ${path_x}, ${path_y}`)
-                    place_fov_flag(path_x, path_y, true);
-                    visible = false;
+    place_fov_flag(pov_x, pov_y, true);
+    const octant_visibility = [true, true, true, true, true, true, true, true];
+    for (var row = 1; row < max_distance; row++) {
+        for (var col = 0; col <= row; col++) {
+            const octants = [[row, col], [-row, col], [row, -col], [-row, -col], [col, row], [-col, row], [col, -row], [-col, -row]];
+            octants.forEach((o, o_index) => {
+                const x = pov_x + o[0];
+                const y = pov_y + o[1];
+                trace(`calculate: ${x},${y}`)
+                if (!octant_visibility[o_index]) {
+                    trace('folse!');
+                    place_fov_flag(x, y, false);
                 } else {
-                    place_fov_flag(path_x, path_y, true);
+                    place_fov_flag(x, y, true);
                 }
-            }
-            path_x = path_x + shortest[0];
-            path_y = path_y + shortest[1];
-            count++;
+                if (get_tile(map, x, y).flags[TileFlags.OPAQUE]) {
+                    trace('opaque');
+                    octant_visibility[o_index] = false;
+                }
+            });
         }
-    });
-    fov_map[pov_x][pov_y] = true;
-    //Build fovmap
-    /*
-    for (let x = camera_x_origin; x < camera_x_origin + width; x++) {
-        fov_map[x] = [];
-        for (let y = camera_y_origin; y < camera_y_origin + height; y++) {
-            fov_map[x][y] = true;
-        }
-    };
-    */
+    }
 
     return fov_map;
 }
